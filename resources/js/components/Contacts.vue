b39b00e378f2255d154eb1ca7d43492211898ad7
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-12-09 08:13:10 -1000

    created vue component

---
+++ resources/js/components/Contacts.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Contacts</h1>
+</template>
+
+<script>
+    export default {
+        data: function(){
+
+        },
+        mounted: function(){
+            console.log('Contacts components loaded....');
+        }
+    }
+</script>
