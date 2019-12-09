b86eaee4e4208382c0f0922007d2dd76e2789e06
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-12-09 08:22:43 -1000

    created add/update contact form

--- resources/js/components/Contacts.vue
+++ resources/js/components/Contacts.vue
@@ -1,14 +1,51 @@
 <template>
-    <h1>Contacts</h1>
+    <div>
+        <h1>Contacts</h1>
+        <form action="#" @submit.prevent="edit ? updateContact(contact.id) : createContact()">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input v-model="contact.name" type="text" class="form-control" name="name">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input v-model="contact.email" type="text" class="form-control" name="email">
+            </div>
+            <div class="form-group">
+                <label for="phone">Phone</label>
+                <input v-model="contact.phone" type="text" class="form-control" name="phone">
+            </div>
+            <div class="form-group">
+                <button v-show="!edit" type="submit" name="button" class="btn btn-primary">New Contact</button>
+                <button v-show="edit" type="submit" name="button" class="btn btn-primary">Update Contact</button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
     export default {
         data: function(){
-
+            return {
+                edit: true,
+                list: [],
+                contact: {
+                    id: '',
+                    name: '',
+                    email: '',
+                    phone: ''
+                }
+            }
         },
         mounted: function(){
             console.log('Contacts components loaded....');
+        },
+        methods: {
+            createContact: function(){
+                console.log('Creating contact...');
+            },
+            updateContact: function(id){
+                console.log('Updating contact...'+id);
+            }
         }
     }
 </script>
